7431f14a3365be6e1a3b925b2ae96185510ce163
ayguillo <ayguillo@student.42.fr>
2020-12-11 14:59:01 +0100

    source level5

---
+++ level5/source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+m = 0
+
+void o() {
+  system("/bin/sh");
+  exit(1)
+}
+
+void n() {
+  char buffer[512];
+
+  fgets(buffer, 512, stdin);
+  printf(buffer)
+  exit(1)
+}
+
+int main() {
+  n ()
+  return ;
+}
